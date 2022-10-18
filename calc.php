ff06f53b4eb33349eda8521ae0212e41639062a5
GabrielHenrique010101 <116092936+GabrielHenrique010101@users.noreply.github.com>
2022-10-18 11:43:57 -0300

    #hacktoberfest

--- calc.php
+++ calc.php
@@ -3,6 +3,6 @@
 $altura = $_POST['txtAltura'];
 $base = $_POST['txtBase'];
 $triangulo = ($base * $altura)/2;
-echo "Área do Triangulo:" . $altura;
+echo "Área do Triangulo desejado é :" . $altura;
 
 ?>
